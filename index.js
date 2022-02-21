4717470336e86bc19028e7c4ff6dbf3e58f0575f
aaron592 <64913505+aaron592@users.noreply.github.com>
2022-02-21 10:23:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+
+function getData(country,topic){
+    document.getElementById("news").innerHTML="";
+
+    fetch("https://newsapi.org/v2/top-headlines?country="+country+"&category="+topic+"&apiKey=b50ec4554a7b41d2a206c8dc819e08ef")
+    .then((response) => response.json())
+    .then((data)=>{
+        let news = data.articles;
+        // console.log(news);
+        
+        news.forEach((n,index)=>{
+            let newsContainer = document.createElement("div");
+            newsContainer.classList.add("news_container");
+    
+            let newsImage = document.createElement("div");
+            newsImage.classList.add("image");
+    
+            let img = document.createElement("img");
+            img.setAttribute("src",n.urlToImage);
+            newsImage.appendChild(img);
+    
+            let newsDetails = document.createElement("div");
+            newsDetails.classList.add("details");
+    
+            let newsH3 = document.createElement("h3");
+            newsH3.classList.add("h3");
+            newsH3.append(n.title)
+    
+            let newsH5 = document.createElement("h5");
+            newsH5.classList.add("h5");
+            newsH5.append(n.description);
+    
+            let newsBtn = document.createElement("button");
+            newsBtn.classList.add("btn");
+            newsBtn.append("Read Full Article")
+    
+            newsDetails.appendChild(newsH3);
+            newsDetails.appendChild(newsH5);
+            newsDetails.appendChild(newsBtn);
+    
+            newsContainer.appendChild(newsImage);
+            newsContainer.appendChild(newsDetails);
+    
+            document.getElementById("news").appendChild(newsContainer);
+        })
+    })
+    .catch(err => console.log(err));
+    
+}
+
+getData("in","business");
+
+function search(){
+    let country = document.getElementById('country').value;
+    let topic = document.getElementById('topic').value;
+    getData(country, topic);
+}
