ece2da9f2073be4527a16ff5b942c7b648eec9c2
aaron592 <64913505+aaron592@users.noreply.github.com>
2022-02-22 20:02:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,16 @@
 function getData(country,topic){
     document.getElementById("news").innerHTML="";
 
-    fetch(`https://newsapi.org/v2/top-headlines?country=${country}&category=${topic}&apiKey=b50ec4554a7b41d2a206c8dc819e08ef`,{method:"GET", credentials:"include"})
+    fetch("https://newsapi.org/v2/top-headlines?country="+country+"&category="+topic+"&apiKey=b50ec4554a7b41d2a206c8dc819e08ef",{
+    method:"GET",
+    credentials:"include"
+    })
     .then((response) => response.json())
     .then((data)=>{
         console.log(data)
         let news = data.articles;
         console.log(news);
-        for(n=0;n<news.length;n++){
+        news.forEach((n,index)=>{
             // console.log(news[n].title);
             let newsContainer = document.createElement("div");
             newsContainer.classList.add("news_container");
@@ -18,7 +21,7 @@ function getData(country,topic){
             newsImage.classList.add("image");
     
             let img = document.createElement("img");
-            img.setAttribute("src",news[n].urlToImage);
+            img.setAttribute("src",n.urlToImage);
             newsImage.appendChild(img);
     
             let newsDetails = document.createElement("div");
@@ -26,11 +29,11 @@ function getData(country,topic){
     
             let newsH3 = document.createElement("h3");
             newsH3.classList.add("h3");
-            newsH3.append(news[n].title)
+            newsH3.append(n.title)
     
             let newsH5 = document.createElement("h5");
             newsH5.classList.add("h5");
-            newsH5.append(news[n].description);
+            newsH5.append(n.description);
     
             let newsBtn = document.createElement("button");
             newsBtn.classList.add("btn");
